82d4641759341fbfefd854a9260d5353b7530804
Min-coder666 <2441238643@qq.com>
2021-03-31 21:58:36 +0800

    RESTfulDemo

---
+++ RESTfulDemo/src/main/java/com/min/RESTfulDemoApplication.java
@@ -0,0 +1,12 @@
+package com.min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RESTfulDemoApplication.class,args);
+    }
+}
+
